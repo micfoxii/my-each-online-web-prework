e8f2827f802bbc044167b15e27733d70a035f424
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-28 16:14:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,10 @@
 def my_each(collection) # put argument(s) here
 i=0 
 while i < collection.length 
-yield(array[i])
+yield(collection[i])
 i += 1 
+end
+collection
 #collection.map do |x|
 #"#{x}"
 #end
